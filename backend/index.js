5e73c9598388c8615dfab7a46c7c9876efa31450
Nilay-Chaudhary <nilaychaudhary04@gmail.com>
2024-06-28 19:39:08 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const connectTo = require('./db.js')
 connectTo();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", "https://movix-mocm.vercel.app/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accept"
